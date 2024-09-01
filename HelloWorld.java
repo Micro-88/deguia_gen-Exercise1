790da276467ee82f8ce28f492e7921e5e41fc56c
Antonio Vicen <ajvicen@gmail.com>
2024-09-01 16:32:46 +0800

    Add enterAge feature

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,16 @@
 import java.util.Scanner;
 
 public class HelloWorld {
+
+    //function to ask for user's age
+    static void enterAge(Scanner scanner) {         
+        // Ask for the user's age
+        System.out.print("Enter Age: ");
+        int age = scanner.nextInt();
+        // Displays user's age
+        System.out.println("Your Age is: " + age + "years old");
+    }
+
     public static void main(String[] args) {
         // Create a Scanner object to read input
         Scanner scanner = new Scanner(System.in);
@@ -12,6 +22,9 @@ public class HelloWorld {
         // Greet the user
         System.out.println("Hello, " + name + "!");
         
+        //call enterAge function to ask for user's age
+        enterAge(scanner);
+
         // Close the scanner to avoid resource leaks
         scanner.close();
     }
